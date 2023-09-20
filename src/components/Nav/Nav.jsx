be4044cfa01b62dcc7cb37cdcc9822bf4294dad1
mp-keyner <keyneroswaldo0@gmail.com>
2023-09-19 20:57:00 -0500

    19 de septiembre

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 const Nav = () => {
+    const [show, setShow] = useState(false)
+    const HandlerMenu = () => {
+        console.log('si', show)
+        setShow(!show)
+    }
   return (
     <nav>
         <div className='listas'>
@@ -14,9 +19,35 @@ const Nav = () => {
         </div>
         <div className='logo'>
             <img src="../../../public/img/Estrella.svg" alt="" />
+            <p>Logo</p>
         </div>
-        <div>
+        <div className='menu'>
+            <img src="../../../public/img/Menu.svg" alt="" onClick={HandlerMenu}/>
+        </div>
+        <div className='Buscar'>
+            <span>
+            <img src="../../../public/img/Lupa.svg" alt="" />
             <input type="text" placeholder='Search for anything' className='IntSear'/>
+            </span>
+        </div>
+        <div className={`MenuDes ${show ? 'Show' : 'hide'} `}>
+        <div className='logo'>
+            <img src="../../../public/img/Estrella.svg" alt="" />
+            <p>Logo</p>
+        </div>
+        <div className='Buscar2'>
+            <span>
+            <img src="../../../public/img/Lupa.svg" alt="" />
+            <input type="text" placeholder='Search for anything' className='IntSear'/>
+            </span>
+        </div>
+        <ul>
+                <li>Home</li>
+                <li>Product</li>
+                <li>About</li>
+                <li>Pricingx</li>
+            </ul>
+            <img src="../../../public/img/x.svg" alt="" className='x' onClick={HandlerMenu}/>
         </div>
     </nav>
   )
